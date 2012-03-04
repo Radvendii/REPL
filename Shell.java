e59363f5384fffa765632cb543035dacead51803
Ta'iř sj Ořit <taeer@necsi.edu>
2012-03-03 20:18:39 -0500

    raptortech97's changes

---
+++ Shell.java
@@ -0,0 +1,32 @@
+public class Shell
+{
+ private final String prompt = ">";
+ 
+  public static void main(String[] args)
+ {
+   StringHolder comms = new StringHolder(); // Initializes StringHolder
+   while(true)
+   {
+     String in = getStringFromShell(prompt);
+     comms.add(in);
+     RunCode run = new RunCode(comms.toFile());
+         System.out.println(comms.toFile());
+                  run.comprun();
+   }
+ }
+ 
+  //get String or simply enter from shell
+ public static String getStringFromShell(String prompt)
+ {
+   try
+   {
+     System.out.print(prompt);
+     return new BufferedReader(new InputStreamReader(System.in)).readLine();   
+   }
+   catch (IOException e)
+   {
+     e.printStackTrace();
+   }
+ return null ;
+ }
+}
